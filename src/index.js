f40735643c6285e45a6720429871db12ece3e04e
aleurdo <7777846@gmail.com>
2020-03-22 18:19:27 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,28 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let open = [],
+        close = [];
+    let arr = expr.split('');
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] === '(') {
+            open.push(arr[i]);
+        }
+        if (arr[i] === ')') {
+            close.push(arr[i]);
+        }
+        if ((arr[i] + arr[i + 1] + arr[i + 2]) === '/ 0') {
+            throw new Error('TypeError: Division by zero.')
+        }
+    }
+    if (open.length === close.length) {
+        return new Function('return ' + expr)();
+    } else {
+        throw ("ExpressionError: Brackets must be paired");
+    }
 }
+
 
 module.exports = {
     expressionCalculator
